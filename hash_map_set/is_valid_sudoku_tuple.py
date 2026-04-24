12de0f50e7368862ed9474a94be2de98e33bcf89
colombefioren <colomberakotonjanahary@gmail.com>
2026-04-24 07:27:03 +0300

    fix: change row col and box value to string to avoid confusion in set

--- hash_map_set/is_valid_sudoku_tuple.py
+++ hash_map_set/is_valid_sudoku_tuple.py
@@ -10,9 +10,9 @@ def is_valid_sudoku(board):
             if current == ".":
                 continue
 
-            row = (current,i)
-            col = (current,j)
-            box= (current,(i//3)*3 +(j//3))
+            row = f"{current} in row {i}"
+            col = f"{current} in col {j}"
+            box= f"{current} in box {(current,(i//3)*3 +(j//3))}"
 
             if row in seen or col in seen or box in seen:
                 return False
@@ -22,3 +22,17 @@ def is_valid_sudoku(board):
             seen.add(box)
 
     return True
+
+
+board = [
+    ["5","3",".",".","7",".",".",".","."],
+    ["6",".",".","1","9","5",".",".","."],
+    [".","9","8",".",".",".",".","6","."],
+    ["8",".",".",".","6",".",".",".","3"],
+    ["4",".",".","8",".","3",".",".","1"],
+    ["7",".",".",".","2",".",".",".","6"],
+    [".","6",".",".",".",".","2","8","."],
+    [".",".",".","4","1","9",".",".","5"],
+    [".",".",".",".","8",".",".","7","9"]
+]
+print(is_valid_sudoku(board)) 
