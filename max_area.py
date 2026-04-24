fa6199c6e49fdc998cd3850132d7b7c924ba3d6b
colombefioren <colomberakotonjanahary@gmail.com>
2026-04-24 11:13:43 +0300

    feat: implement max area

---
+++ max_area.py
@@ -0,0 +1,28 @@
+def max_area(height):
+    left,right = 0, len(height) - 1
+    distance = len(height) - 1
+    max_area=min(height[left],height[right]) * distance
+    while left < right:
+
+        if(height[left] < height[right]):
+            left += 1
+            distance -= 1
+        
+
+        elif(height[left] > height[right]):
+            right -= 1
+            distance -= 1
+
+        else:
+            right-=1
+            left+=1
+            distance-=2
+
+        current_area=min(height[left],height[right]) * distance
+        if(current_area>max_area):
+                max_area=current_area
+
+    return max_area
+        
+
+print(max_area([1,2,3,1000,9]))
