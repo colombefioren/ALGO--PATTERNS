e756b7fefaec8bacb5080ab7de2f2e5a7f755bb9
colombefioren <colomberakotonjanahary@gmail.com>
2026-05-17 10:08:52 +0300

    fix: longest palindrome

--- string/longest_palindrome.py
+++ string/longest_palindrome.py
@@ -1,21 +1,24 @@
-def longest_palindrome(s):
-    l=0
-    r=l+1
-    max_str=""
-    f=False
-    while r<len(s):
-        if s[l]==s[r]:
-            if len(max_str)<r-l+1:
-                max_str=s[l:r+1]
-                f=True
-            r+=1
-        if s[l]!=s[r]:
-            if f:
-                l += 1
-            else:
-                r += 1
-            f=False
-    return max_str
+def longest_palindrome(s:str)->str:
+    start = 0
+    end = 0
+    for i in range(len(s)):
+        len_odd = expand(s,i,i)
+        len_even = expand(s,i,i+1)
 
-print(longest_palindrome("mlml"))
+        current_len = max(len_even,len_odd)
+
+        if current_len > end - start:
+            start = i - (current_len - 1) //2 
+            end = i + (current_len // 2)
+
+    return s[start:end+1]
+
+def expand(s:str,left:int,right:int)->int:
+    while left >= 0 and right < len(s) and s[left] == s[right]:
+        left -= 1
+        right += 1
+    return right - left - 1
+
+print(longest_palindrome("babad"))
+
 
